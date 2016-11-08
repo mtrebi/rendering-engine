c29e7f834c9329d08d0c60eedf946d5f7e86b944
Maru <marianotrebino@gmail.com>
2016-11-08 18:28:23 -0500

    Hello window tutorial from learnopengl.com.

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,58 @@
-
+// GLEW: Include glew before glfw3 because it defines the impl. of OpenGL
+#include <GL/glew.h>
+// GLFW
 #include <GLFW/glfw3.h>
-#include <stdlib.h>
-#include <stdio.h>
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
+#include <iostream>
+
+/**
+    Initialize Glew
+*/
+void initializeGlew(){
+    // Enable experimental features
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK){
+        std::cerr << "Failed to inicitalize GLEW" << std::endl;
+    }
 }
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
+
 int main(void)
 {
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
+    // Initialize glfw (memory allocation)
+    glfwInit();
+    // Set OpenGL version to 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Set working mode to core-profile (disable legacy calls to immediate-mode)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Window Title", nullptr, nullptr);
+    if (window == nullptr){
+        std::cerr << "Failted to create GLFW window" << std::endl;
+        // Destroys all dynamic allocated memory for GLFW
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        return -1;
     }
+
     glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
-        glfwSwapBuffers(window);
+       initializeGlew();
+
+    // Tell openGl the size of the rendering display
+    int width, height;
+    // Get the window parameters
+    glfwGetFramebufferSize(window, &width, &height);
+    // Set viewport at location 0,0
+    glViewport(0, 0, width, height);
+
+    // Run game loop
+    while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
+        // Swap front and back buffer (Double-buffer technique)
+        glfwSwapBuffers(window);
     }
-    glfwDestroyWindow(window);
+
     glfwTerminate();
-    exit(EXIT_SUCCESS);
+
+    return 0;
 }
