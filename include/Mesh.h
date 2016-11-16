b06120019c2305ea06f78870012cc583d0d2376c
Maru <marianotrebino@gmail.com>
2016-11-16 12:16:32 -0500

    Added Mesh class.

---
+++ include/Mesh.h
@@ -0,0 +1,36 @@
+#include <glm/glm.hpp>
+#include <vector>
+#include "shaders/Shader.h"
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture {
+	GLuint id;
+	enum { DIFFUSE, SPECULAR } type;
+};
+
+class Mesh {
+public:
+	/* Mesh Data */
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+	std::vector<Texture> textures;
+
+private:
+	/* Render Data */
+	GLuint 	m_VAO, 	// Stores data
+			m_VBO, 	// Data configuration (enable/disable attr)
+			m_EBO;	// Data indices (order)
+
+public:
+	Mesh(std::vector<Vertex> &vertices, std::vector<GLuint> &indices, std::vector<Texture> &textures);
+	void draw(Shader shader);
+private:
+	// Initialize buffers
+	void initialize();
+
+};
---
+++ src/Mesh.cpp
@@ -0,0 +1,77 @@
+#include "Mesh.h"
+#include <string>
+
+Mesh::Mesh(std::vector<Vertex> &vertices, std::vector<GLuint> &indices, std::vector<Texture> &textures){
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	this->initialize();
+}
+
+void Mesh::initialize(){
+	// Generate ids
+    glGenVertexArrays(1, &m_VAO);
+    glGenBuffers(1, &m_VBO);
+    glGenBuffers(1, &m_EBO);
+
+    // Bind
+    glBindVertexArray(m_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+
+    // Buffer data
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+
+    // Setup for drawing
+    	// Vertex Positions
+    glEnableVertexAttribArray(0);	
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 
+                         (GLvoid*)0);
+    	// Vertex Normals
+    glEnableVertexAttribArray(1);	
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 
+                         (GLvoid*)offsetof(Vertex, Normal));
+    	// Vertex Texture Coords
+    glEnableVertexAttribArray(2);	
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), 
+                         (GLvoid*)offsetof(Vertex, TexCoords));
+
+    // Unbind
+    glBindVertexArray(0);
+}
+
+//TODO: move this to a Shader class that gets a Mesh object!!!
+void Mesh::draw(Shader shader){
+	GLuint diffuseCount = 1;
+	GLuint specularCount = 1;
+
+	// Assign textures dynamically
+	for (int i = 0; i < textures.size(); ++i){
+		glActiveTexture(GL_TEXTURE0 + i);
+
+		Texture currentTexture = textures[i];
+
+		std::string texture_suffix = "texture";
+		switch(currentTexture.type){
+			case Texture::DIFFUSE:
+				texture_suffix = "_diffuse" + diffuseCount;
+				++diffuseCount;
+				break;
+			case Texture::SPECULAR:
+				texture_suffix = "_specular" + specularCount;
+				++specularCount;
+				break;
+		}
+
+    	glUniform1i(glGetUniformLocation(shader.Program, ("material." + texture_suffix).c_str()), i);
+    	glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+	glActiveTexture(GL_TEXTURE0);
+
+	// Draw mesh
+	glBindVertexArray(m_VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);	
+}
