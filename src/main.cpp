82031c22adc9ce3e5a0eb75233ecb254b5b4ab38
Mtrebi <marianotrebino@gmail.com>
2017-03-21 11:43:18 +0100

    Refactor code to start Lighting tutorials

--- src/main.cpp
+++ src/main.cpp
@@ -17,46 +17,167 @@
 
 // Other includes
 #include "shaders/Shader.h"
+#include "Camera.h"
+#include "Model.h"
 
-const GLchar* vs_path = "src/shaders/default.vs";
-const GLchar* fs_path = "src/shaders/default.fs";
+// Function prototypes
+GLFWwindow* initialize(const GLuint width, const GLuint height);
 
-// Forward declarations
+void terminate();
+void clearBuffers();
+void calculateFrameTime();
+void calculateCameraMovement();
+void setupData();
+void setupOpenGLFlags();
+void setupProjectionViewMatrix(Shader shader);
+void setupModelMatrix(Shader shader, glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3 translate = glm::vec3(1.0f, 1.0f, 1.0f));
+
+// Callbacks
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+// Attributes
+const GLuint SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool keys[1024];
+GLfloat lastX = 400,        // Mouse last X position
+lastY = 300;        // Mouse last Y position
+GLfloat deltaTime = 0.0f,   // Time between current frame and last frame
+lastFrame = 0.0f;   // Time of last frame
+bool firstMouse = false;
+
+const GLchar* VS_PATH = "shaders/default.vs";
+const GLchar* FS_PATH = "shaders/default.fs";
+
+const GLchar* LIGHT_VS_PATH = "src/shaders/light_shader.vs";
+const GLchar* LIGHT_FS_PATH = "src/shaders/light_shader.fs";
 
 
+GLchar* CUBE_PATH = "assets/models/box/box.obj";
 
+GLuint light_VAO;
 
+glm::vec3 light_position = glm::vec3(1.2f, 1.0f, 2.0f);
+
+//GLuint VBO, lightVAO, cubeVAO, diffuseTexture, specularTexture, uboMatrices;
 
 // The MAIN function, from here we start the application and run the game loop
-int main(){
-  
-  
+int main() {
+
+  GLFWwindow* window = initialize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  setupOpenGLFlags();
+
+  setupData();
+
+  //Shader default_shader = Shader(VS_PATH, FS_PATH);
+  Shader light_shader = Shader(LIGHT_VS_PATH, LIGHT_FS_PATH);
+
+  // Game loop
+  while (!glfwWindowShouldClose(window))
+  {
+    // Set frame time
+    calculateFrameTime();
+    glfwPollEvents();
+    calculateCameraMovement();
+    clearBuffers();
+
+    light_shader.Use();
+
+    setupProjectionViewMatrix(light_shader);
+    setupModelMatrix(light_shader, glm::vec3(0.2f), light_position);
+
+    glBindVertexArray(light_VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+
+    glfwSwapBuffers(window);
+  }
+
+
+  terminate();
+  return 0;
+}
+
+GLFWwindow* initialize(const GLuint width, const GLuint height) {
+  // Init GLFW
   glfwInit();
-  // OpenGL version 3.3
+  // Set all the required options for GLFW
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-  GLFWwindow * window = glfwCreateWindow(800, 600, "Tutorial_1", nullptr, nullptr);
-  if (window == nullptr) {
-    std::cerr << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-
+  // Create a GLFWwindow object that we can use for GLFW's functions
+  GLFWwindow* window = glfwCreateWindow(width, height, "rendering-engine", nullptr, nullptr);
   glfwMakeContextCurrent(window);
+
+  // Set the required callback functions
   glfwSetKeyCallback(window, key_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
 
-  // Initialize GLEW pointers to OpenGL functions
+  // Options
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
   glewExperimental = GL_TRUE;
-  if (glewInit() != GLEW_OK) {
-    std::cerr << "Failed to initialize GLEW" << std::endl;
-    return -1;
-  }
+  glewInit();
 
-  // Setup Vertex Data
+  // Define the viewport dimensions
+  glViewport(0, 0, width, height);
+
+  return window;
+}
+
+void terminate() {
+  /*
+  glDeleteVertexArrays(1, &lightVAO);
+  glDeleteVertexArrays(1, &cubeVAO);
+  glDeleteBuffers(1, &VBO);
+  */
+  glfwTerminate();
+}
+
+void setupOpenGLFlags() {
+  glEnable(GL_DEPTH_TEST);
+  //glEnable(GL_STENCIL_TEST);
+  //glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+}
+
+void clearBuffers() {
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+}
+
+void calculateFrameTime() {
+  GLfloat currentFrame = glfwGetTime();
+  deltaTime = currentFrame - lastFrame;
+  lastFrame = currentFrame;
+}
+
+void calculateCameraMovement() {
+  // Camera controls
+  if (keys[GLFW_KEY_W])
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+  if (keys[GLFW_KEY_S])
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+  if (keys[GLFW_KEY_A])
+    camera.ProcessKeyboard(LEFT, deltaTime);
+  if (keys[GLFW_KEY_D])
+    camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void setupData() {
+  /*
+  glGenBuffers(1, &uboMatrices);
+  glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+  glBufferData(GL_UNIFORM_BUFFER, 2 * sizeof(glm::mat4), NULL, GL_STATIC_DRAW);
+  glBindBuffer(GL_UNIFORM_BUFFER, 0);
+  glBindBufferBase(GL_UNIFORM_BUFFER, 0, uboMatrices);
+  */
+
   GLfloat vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -100,136 +221,78 @@ int main(){
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
   };
-  GLuint indices[] = {  // Note that we start from 0!
-    0, 1, 3, // First Triangle
-    1, 2, 3  // Second Triangle
-  };
 
-  // Setup Vertex Buffer Object
-  GLuint VBO, VAO, EBO;
+  GLuint VBO;
   glGenBuffers(1, &VBO);
-  //glGenBuffers(1, &EBO);
-
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  // 0. Copy our vertices array in a buffer for OpenGL to use
-  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-  //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  glGenVertexArrays(1, &light_VAO);
+  glBindVertexArray(light_VAO);
+
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  // 1. Then set the vertex attributes pointers
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
-  glEnableVertexAttribArray(1);
-
-  //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-  //glEnableVertexAttribArray(2);
-
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
-
-  int texture_width1, texture_height1;
-  int texture_width2, texture_height2;
-
-
-  GLuint texture1, texture2;
-  glGenTextures(1, &texture1);
-  glBindTexture(GL_TEXTURE_2D, texture1);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  unsigned char* image1 = SOIL_load_image("assets/textures/container.jpg", &texture_width1, &texture_height1, 0, SOIL_LOAD_RGB);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width1, texture_height1, 0, GL_RGB, GL_UNSIGNED_BYTE, image1);
-  glGenerateMipmap(GL_TEXTURE_2D);
-  SOIL_free_image_data(image1);
-  glBindTexture(GL_TEXTURE_2D, 0);
-
-  glGenTextures(1, &texture2);
-  glBindTexture(GL_TEXTURE_2D, texture2);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  unsigned char* image2 = SOIL_load_image("assets/textures/awesomeface.png", &texture_width2, &texture_height2, 0, SOIL_LOAD_RGB);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width2, texture_height2, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
-  glGenerateMipmap(GL_TEXTURE_2D);
-  SOIL_free_image_data(image2);
-  glBindTexture(GL_TEXTURE_2D, 0);
-
-  // 2. Setup Shaders
-  Shader shader(vs_path, fs_path);
-
-  // Setup OpenGL viewport with Window sizes
-  int width, height;
-  glfwGetFramebufferSize(window, &width, &height);
-
-  // This info is used to transform from NDC to Screen Coordinates through Viewport Transform
-  glViewport(0, 0, width, height);
-
-
-  glEnable(GL_DEPTH_TEST);
-
-  while (!glfwWindowShouldClose(window)) {
-    // Handle user input (keyboard, mouse...)
-    glfwPollEvents();
-
-    // Rendering commands go here...
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    shader.Use();
-    // Bind Textures using texture units
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glUniform1i(glGetUniformLocation(shader.Program, "ourTexture1"), 0);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    glUniform1i(glGetUniformLocation(shader.Program, "ourTexture2"), 1);
-
-    glm::mat4 model;
-    model = glm::rotate(model, sin((GLfloat)glfwGetTime()), glm::vec3(0.0f, 1.0f, 0.0f));
-    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
-    glm::mat4 view;
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
-    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-
-    glm::mat4 project;
-    project = glm::perspective(glm::radians(45.0f), (float) width / height, 0.1f, 100.0f);
-    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(project));
-
-
-
-
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-
-    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
-
-
-    // Swap color buffers (uses double buffering -> front and back)
-    glfwSwapBuffers(window);
-  }
-
-  // Delete all resources
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  //glDeleteBuffers(1, &EBO);
-
-  glfwTerminate();
-  return 0;
 }
 
+void setupProjectionViewMatrix(Shader shader) {
+  /*
+  glUniformBlockBinding(shader.Program, glGetUniformBlockIndex(shader.Program, "Matrices"), 0);
+
+  glm::mat4 projection = glm::perspective(45.0f, (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+  glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+  glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(projection));
+  glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+  glm::mat4 view = camera.GetViewMatrix();
+  glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+  glBufferSubData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), sizeof(glm::mat4), glm::value_ptr(view));
+  glBindBuffer(GL_UNIFORM_BUFFER, 0);
+  */
+  glm::mat4 projection;
+  projection = glm::perspective(45.0f, (float) SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 100.0f);
+  glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+  glm::mat4 view = camera.GetViewMatrix();
+  glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+}
+
+void setupModelMatrix(Shader shader, glm::vec3 scale, glm::vec3 translate) {
+  glm::mat4 model = glm::mat4();
+  model = glm::translate(model, translate);
+  model = glm::scale(model, scale);
+  glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+}
+
+// Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-  // When a user presses the escape key, we set the WindowShouldClose property to true, 
-  // closing the application
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
+  if (key >= 0 && key < 1024)
+  {
+    if (action == GLFW_PRESS)
+      keys[key] = true;
+    else if (action == GLFW_RELEASE)
+      keys[key] = false;
+  }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+  if (firstMouse)
+  {
+    lastX = xpos;
+    lastY = ypos;
+    firstMouse = false;
+  }
+
+  GLfloat xoffset = xpos - lastX;
+  GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+
+  lastX = xpos;
+  lastY = ypos;
+
+  camera.ProcessMouseMovement(xoffset, yoffset);
 }
